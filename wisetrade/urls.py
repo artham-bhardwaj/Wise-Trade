83a97cbb0df2e8e6be15a026c1e5c13abcd467c0
Ayush Anand <ayushanand2019264@gmail.com>
2025-04-09 14:54:17 +0530

    dashboard , added new stocks ,added recommendation , add watchlist

--- wisetrade/urls.py
+++ wisetrade/urls.py
@@ -3,11 +3,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from dashboard.views import price_data_view
+from technical_analysis.views import fetch as technical_analysis_view
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('api/price_data/', price_data_view, name='price_data'),
-    # path('',views.homepage,name='homepage')
+    path('api/technical/', technical_analysis_view, name='technical_analysis'),
 ]
